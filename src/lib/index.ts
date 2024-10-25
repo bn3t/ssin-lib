9ce2220e39136dab75ccb17c91b68886bb6ffac1
Bernard Niset <bernard.niset@gmail.com>
2024-10-25 21:54:33 +0200

    fix: Add more exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
 export { SSIN } from '@/lib/SSIN';
+export * from '@/lib/types';
+export * from '@/lib/SSINExtractorHelper';
+export * from '@/lib/SSINValidatorHelper';
+export * from '@/lib/SSINGeneratorHelper';
